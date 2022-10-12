9ad33d257959f243c7b21b4c73141b3d4517436d
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-11 22:32:04 -0600

    working search history appending buttons and icons linked

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,17 @@
-var cityContainer = document.querySelector("#cityContainer");
-var localStg = [];
+var savedCity = document.querySelector("#savedCity");
+var storage = [];
 
-displayCity();
+displayCityName();
 
 var weather={
-
-
-    fetchWeather:function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ba45cee755183b20520597d1996ada97")
+    getWeather:function(city){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=ba45cee755183b20520597d1996ada97')
         .then( (response) => {
             if (response.ok) {
-                if(localStg.includes(city) === false){
-                    localStg[localStg.length] = city;
-                    console.log(localStg);
-                    localStorage.setItem("cities", JSON.stringify(localStg));
+                if(storage.includes(city) === false){
+                    storage[storage.length] = city;
+                    localStorage.setItem("cities", JSON.stringify(storage));
                 }
-
-
                 response.json().then((data) =>{
                 this.displayWeather(data);
               });
@@ -26,48 +21,45 @@ var weather={
             }
           })
     },
-    displayWeather:function(weatherDt) {
-        var {name} = weatherDt;
-        var {country} = weatherDt.sys;
-        var {icon} = weatherDt.weather[0];
-        var {temp,humidity} = weatherDt.main;
-        var {speed} = weatherDt.wind;
-       
+    displayWeather:function(todayWeather) {
+        var {name} = todayWeather;
+        var {icon} = todayWeather.weather[0];
+        var {temp,humidity} = todayWeather.main;
+        var {speed} = todayWeather.wind;
 
-        document.querySelector(".city").innerText = "Weather today in " + name + " / " + country;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
-        document.querySelector(".temp").innerText = Math.round(temp) + "°F";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind: " + speed + "MPH";
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +'.png';
+        document.querySelector(".temp").innerText = Math.round(temp) + '°F';
+        document.querySelector(".humidity").innerText = 'Humidity: ' + humidity + '%';
+        document.querySelector(".wind").innerText = 'Wind: ' + speed + "MPH";
 
     },
 
-
-
-    fetchWeatherW:function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ city + "&units=imperial&appid=ba45cee755183b20520597d1996ada97")
+    getWeeklyWeather:function(city) {
+        fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=ba45cee755183b20520597d1996ada97')
         .then((response) => response.json())
-        .then((data) => this.displayWeatherW(data));
+        .then((data) => this.weeklyWeather(data));
 
     },
-    displayWeatherW:function(data) {
+    weeklyWeather:function(data) {
         for (var i = 0; i < 5; i++){
-            var {icon} = data.list[i*8].weather[0];
-            var {temp, humidity} = data.list[i*8].main;
-            var { speed } = data.list[i*8].wind;
+            var {icon, description} = data.list[i * 8].weather[0];
+            var {temp, humidity} = data.list[i * 8].main;
+            var { speed } = data.list[i * 8].wind;
 
-        document.querySelector("#icon"+(i) ).src = "https://openweathermap.org/img/wn/" + icon +".png";
-        document.querySelector("#temp"+(i)).innerText = Math.round(temp) + "°F";
-        document.querySelector("#humidity"+(i)).innerText = "Humidity: " + humidity + "%";
-        document.querySelector("#wind"+(i)).innerText = "Wind: " + speed + "MPH";
+        document.querySelector("#icon"+(i) ).src = "https://openweathermap.org/img/wn/" + icon +'.png';
+        document.querySelector("#description"+(i)).innerText = description;
+        document.querySelector("#temp"+(i)).innerText = Math.round(temp) + '°F';
+        document.querySelector("#humidity"+(i)).innerText = 'Humidity: ' + humidity + '%';
+        document.querySelector("#wind"+(i)).innerText = 'Wind: ' + speed + 'MPH';
         }
-        displayCity();
+        displayCityName();
     },
 
     search:function(){
 
-        this.fetchWeather(document.querySelector(".search-bar").value);
-        this.fetchWeatherW(document.querySelector(".search-bar").value);
+        this.getWeather(document.querySelector(".search-bar").value);
+        this.getWeeklyWeather(document.querySelector(".search-bar").value);
     }
 };
 document.querySelector(".search button").addEventListener("click", function(){
@@ -82,35 +74,35 @@ document.querySelector(".search-bar").addEventListener("keyup",function(event){
 
 
 
-/*-------------------------WEEKLY weather info-----------------------------*/
-function displayCity() { // save in the array the localStorage information
+
+function displayCityName() {
     var storedCities = JSON.parse(localStorage.getItem("cities"));
     if (storedCities !== null) {
-        localStg = storedCities;
+        storage = storedCities;
         addCityDisplay()
     }    
 }
 function addCityDisplay() {
-    cityContainer.innerHTML = "";
-    for (var i = 0; i < localStg.length; i++){
-        var stgCity = localStg[i];
+    savedCity.innerHTML = "";
+    for (var i = 0; i < storage.length; i++){
+        var storageCity = storage[i];
 
-        var button = document.createElement("button");
-        button.textContent = stgCity;
-        button.addEventListener("click", function(event){// check with TA line 115
-            var stgCity = event.target.textContent;
-            console.log(stgCity);
-            weather.fetchWeather(stgCity);
-            weather.fetchWeatherW(stgCity);
+        var historyButton = document.createElement("button");
+        historyButton.textContent = storageCity;
+        historyButton.addEventListener("click", function(event){
+            var storageCity = event.target.textContent;
+            console.log(storageCity);
+            weather.getWeather(storageCity);
+            weather.getWeeklyWeather(storageCity);
         });
 
-        button.setAttribute("class","btn");
-        // console.log(button);
+        historyButton.setAttribute("class","btn");
 
-        cityContainer.appendChild(button);
+
+        savedCity.appendChild(historyButton);
     }
 }
-displayCity();
+displayCityName();
 
 var currentTime = function() {
 
@@ -118,10 +110,10 @@ var currentTime = function() {
     var date = document.querySelectorAll("p");
     for (var i =0; i<date.length;i++) {
         var currentDate = moment();
-        // console.log(currentDate.toString());
+
         currentDate.add(i,"days");
         currentDate = moment(currentDate).format("MM/DD/YYYY");
-        // console.log(currentDate);
+
         date[i].innerText = currentDate;
 
         
